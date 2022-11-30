dd4875772c12c1ffae27565ecae79df9e058e6fd
WesleyAugusto <ranzerap@gmail.com>
2022-11-30 14:48:50 -0300

    adicinando kafka

--- src/main/kotlin/br/com/curso/service/VendaService.kt
+++ src/main/kotlin/br/com/curso/service/VendaService.kt
@@ -4,14 +4,21 @@ import br.com.curso.dto.input.VendaInput
 import br.com.curso.dto.output.Parcelas
 import br.com.curso.dto.output.Vendas
 import br.com.curso.http.VeiculoHttp
+import br.com.curso.producer.VendaProducer
+import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.inject.Singleton
 import java.time.LocalDate
+import java.util.UUID
 
 
 @Singleton
-class VendaService(private val veiculoService: VeiculoService) {
+class VendaService(
+    private val veiculoService: VeiculoService,
+    private  val vendaProducer: VendaProducer,
+    private val objectMapper: ObjectMapper
+) {
 
-    fun realizarVenda(vendaInput: VendaInput) {
+    fun realizarVenda(vendaInput: VendaInput): Vendas {
         val veiculo = veiculoService.getVeiculo(vendaInput.veiculo)
         val parcelas: ArrayList<Parcelas> = arrayListOf()
         val valorParcelas = vendaInput.valor.divide(vendaInput.quantidadedeParcelas.toBigDecimal())
@@ -24,5 +31,13 @@ class VendaService(private val veiculoService: VeiculoService) {
         }
         val venda = Vendas(vendaInput.cliente, veiculo, vendaInput, parcelas)
             println(venda)
+            confirmaVenda(venda)
+        return venda;
+    }
+
+    fun confirmaVenda(venda:Vendas){
+        var vendaJSON = objectMapper.writeValueAsString(venda)
+        vendaProducer.publicarVenda(UUID.randomUUID().toString(), vendaJSON)
+
     }
 }
